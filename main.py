e5349616eb9b22fb051f0269bd6867629ce5457a
HaohanTsao <andy94729@gmail.com>
2023-10-23 12:12:00 +0800

    build trainning loop

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from random import seed, random
 import numpy as np
 import pandas as pd
 from PIL import Image
+import datetime
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
@@ -96,8 +97,14 @@ class DogCatLoader(Dataset):
         return datasetRGB
 
 # %%
+# %%
+# edit here if needed
+image_size = 224
+image_channel = 3 # RGB
+batch_size = 64
+# %%
 train_transform = transforms.Compose([
-    transforms.RandomResizedCrop(224),
+    transforms.RandomResizedCrop(image_size),
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(15),
     transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), shear=0.1),
@@ -108,7 +115,7 @@ train_transform = transforms.Compose([
 
 valid_transform = transforms.Compose([
     transforms.Resize(256),
-    transforms.CenterCrop(224),
+    transforms.CenterCrop(image_size),
     transforms.ToTensor(),
     # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) params from Imagenet
     transforms.Normalize(mean=[0.5]*3, std=[0.5]*3)
@@ -125,53 +132,110 @@ train_data, valid_data, train_label, valid_label = train_test_split(
 train_dataset = DogCatLoader(train_data, train_transform)
 valid_dataset = DogCatLoader(valid_data, valid_transform)
 
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-valid_loader = DataLoader(valid_dataset, batch_size=64, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
+# %%
+# setting GPU
+device = (torch.device('cuda') if torch.cuda.is_available()
+          else torch.device('cpu'))
+print(f"train on device: {device}")
+
 
 # %%
-image_size = 224
-image_channel = 3 # RGO
-bat_size = 64 # 64
-# %%
-# building Network
-
+# model
 class ImageClassifier(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes, image_size, image_channel):
         super().__init__()
         # input_size: 3*224*224
         self.conv1 = nn.Sequential(
             nn.Conv2d(image_channel, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
             nn.BatchNorm2d(32),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Dropout(0.2),
             ) # 32*112*112
         
         self.conv2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
             nn.BatchNorm2d(64),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Dropout(0.2),
             ) # 64*56*56
         
         self.conv3 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(),
             nn.BatchNorm2d(128),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Dropout(0.2), 
             ) # 128*28*28
         
         self.conv4 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(),
             nn.BatchNorm2d(256),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             nn.Dropout(0.2)
-            ) # 128*28*28
-
+            ) # 256*14*14
         
+        self.fc1 = nn.Sequential(
+            nn.Linear(256*(image_size//16)*(image_size//16), 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            )
+
+        self.fc2 = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        output = self.conv1(x)
+        output = self.conv2(output)
+        output = self.conv3(output)
+        output = self.conv4(output)
+        output.view(-1, 256*(image_size//16)*(image_size//16)) # flatten
+        output = self.fc1(output)
+        output = self.fc2(output)
+
+        return output
+# %%
+# validation def
+def validate(model, train_loader, val_loader):
+    for name, loader in [("train", train_loader), ("val_loader", val_loader)]:
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for imgs, labels in loader:
+                outputs = model(imgs)
+                _, predicted = torch.max(outputs, dim=1)
+
+                total += labels.shape[0]
+                correct += int((predicted == labels).sum())
+
+        print(f"Accuracy {name}: {correct/total:.3f}")
+# %%
+# training loop def
+def training_loop(n_epochs, optimizer, model, loss_fn, train_loader, val_loader):
+    for epoch in range(n_epochs):
+        loss_train = 0.0
+        for images, labels in train_loader:
+            images = images.to(device=device)
+            labels = labels.to(device=device)
+            outputs = model(images)  # one batch at a time
+            loss = loss_fn(outputs, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            loss_train += loss.item()
+
+        if epoch == 0 or (epoch + 1) % 10 == 0:
+            print(f'{datetime.datetime.now} Epoch {epoch}, Trainning loss: {loss_train/(len(train_loader))}')
+            validate(model, train_loader, val_loader)
+
+# %%
+model = ImageClassifier(2, image_size, image_channel).to(device=device)
+loss_fn = nn.BCELoss()
+optimizer = optim.AdamW(model.parameters(), lr=0.001)
 
 # %%
 
