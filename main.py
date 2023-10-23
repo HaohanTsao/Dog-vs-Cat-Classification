b4d9606f6566597129b5cfb78e39d9c7b17244da
HaohanTsao <andy94729@gmail.com>
2023-10-23 15:11:57 +0800

    pipeline completed

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from random import seed, random
 import numpy as np
 import pandas as pd
 from PIL import Image
-import datetime
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
@@ -95,8 +94,6 @@ class DogCatLoader(Dataset):
             ):  # Check Channels
                 datasetRGB.append(dataset[index])
         return datasetRGB
-
-# %%
 # %%
 # edit here if needed
 image_size = 224
@@ -139,7 +136,6 @@ valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
 device = (torch.device('cuda') if torch.cuda.is_available()
           else torch.device('cpu'))
 print(f"train on device: {device}")
-
 
 # %%
 # model
@@ -199,44 +195,113 @@ class ImageClassifier(nn.Module):
 
         return output
 # %%
-# validation def
-def validate(model, train_loader, val_loader):
-    for name, loader in [("train", train_loader), ("val_loader", val_loader)]:
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for imgs, labels in loader:
-                outputs = model(imgs)
-                _, predicted = torch.max(outputs, dim=1)
-
-                total += labels.shape[0]
-                correct += int((predicted == labels).sum())
-
-        print(f"Accuracy {name}: {correct/total:.3f}")
-# %%
 # training loop def
-def training_loop(n_epochs, optimizer, model, loss_fn, train_loader, val_loader):
+from tqdm import tqdm
+def training_loop(n_epochs, optimizer, scheduler, model, loss_fn, train_loader, val_loader, patience = 5):
+    best_val_accuracy = 0.0  
+    early_stopping_patience = patience
+    no_improvement_counter = 0
     for epoch in range(n_epochs):
         loss_train = 0.0
-        for images, labels in train_loader:
-            images = images.to(device=device)
-            labels = labels.to(device=device)
-            outputs = model(images)  # one batch at a time
-            loss = loss_fn(outputs, labels)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            loss_train += loss.item()
+        with tqdm(train_loader, desc=f'Epoch {epoch+1}/{n_epochs}', unit='batch') as t:
+            for images, labels in t:
+                images = images.to(device=device)
+                labels = labels.to(device=device)
+                outputs = model(images)  # one batch at a time
+                loss = loss_fn(outputs, labels)
+                optimizer.zero_grad()
 
-        if epoch == 0 or (epoch + 1) % 10 == 0:
-            print(f'{datetime.datetime.now} Epoch {epoch}, Trainning loss: {loss_train/(len(train_loader))}')
-            validate(model, train_loader, val_loader)
+                loss.backward()
+                optimizer.step()
+                loss_train += loss.item()
+
+                val_accuracy, val_loss, train_accuracy = validate(model, train_loader, val_loader, loss_fn)
+                current_lr = optimizer.param_groups[0]['lr']
+                scheduler.step(val_loss)
+
+        if epoch == 0 or (epoch + 1) % 5 == 0:
+            print(f'Epoch {epoch}, loss: {loss_train/(len(train_loader))}, acc: {train_accuracy}, val_loss: {val_loss/(len(val_loader))}, val_acc: {val_accuracy}, lr:{current_lr}')
+        
+        if val_accuracy > best_val_accuracy:
+            best_val_accuracy = val_accuracy
+        else: 
+            no_improvement_counter += 1
+
+        if no_improvement_counter >= early_stopping_patience:
+            print(f"Early stopping after {epoch+1} epochs.")
+            break
+# %%
+# validation def
+def validate(model, train_loader, val_loader, loss_fn, testing = False):
+    for name, loader in [("train", train_loader), ("val", val_loader)]:
+        if name == "train" and testing == True:
+            train_accuracy = None
+            continue
+        else:
+            correct = 0
+            total = 0
+            total_loss = 0
+            with torch.no_grad():
+                for imgs, labels in loader:
+                    outputs = model(imgs)
+                    _, predicted = torch.max(outputs, dim=1)
+
+                    total += labels.shape[0]
+                    correct += int((predicted == labels).sum())
+                    loss = loss_fn(outputs, labels)
+                    total_loss += loss.item()
+
+            if name == "val":
+                val_accuracy = correct / total
+                val_loss = val_loss
+            
+            if name == "train":
+                train_accuracy = correct / total
+    
+    return val_accuracy, val_loss, train_accuracy    
 
 # %%
 model = ImageClassifier(2, image_size, image_channel).to(device=device)
 loss_fn = nn.BCELoss()
 optimizer = optim.AdamW(model.parameters(), lr=0.001)
-
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', patience=2, factor=0.5, verbose=True, min_lr=0.00001)
 # %%
-
+model.train()
+training_loop(
+    n_epochs=30,
+    optimizer=optimizer,
+    scheduler=scheduler,
+    model=model,
+    loss_fn=loss_fn,
+    train_loader=train_loader,
+    val_loader=valid_loader,
+    patience=5
+)
 # %%
+# testing
+testing_set = ImageFolder(test_folder)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+# %%
+model.eval()
+test_loss = 0
+correct = 0
+test_accuracy, test_loss, _ = validate(model, train_loader, test_loader, loss_fn, testing=True)
+
+print(f"Average Loss: {test_loss / len(test_loader)}, Accuracy: {test_accuracy}")
+# %%
+# save model
+torch.save(
+        {
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+        },
+        "model_pth",
+    )
+# %%
+# load_model
+# loaded_model = ImageClassifier()
+# optimizer = torch.optim.Adam(loaded_model.parameters())  
+# checkpoint = torch.load('model_pth')
+
+# loaded_model.load_state_dict(checkpoint['model_state_dict'])
+# optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
